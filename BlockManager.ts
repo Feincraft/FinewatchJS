7eb4d028fb85d22c657cb24ef1469777b134597e
Ilya <32096531+Feincraft@users.noreply.github.com>
2021-12-16 17:58:23 +0100

    Update BlockManager.ts

--- BlockManager.ts
+++ BlockManager.ts
@@ -1,6 +1,11 @@
 //% color=190 weight=500 icon="\uf121" block="Finewatch Apps"
 namespace FinewatchApps {
     //% block
+    export function addWatchApp(): void {
+        TaskManager.installedApps.push(new AppWatch())
+    }
+
+    //% block
     export function addCompassApp(): void {
         TaskManager.installedApps.push(new AppCompass())
     }
